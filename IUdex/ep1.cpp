dc185f2bcf4c56d3700eadd266b843acec508b2c
Bezerra <54821094+jpbezerra@users.noreply.github.com>
2024-06-01 15:15:40 -0300

    Add files via upload

---
+++ IUdex/ep1.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+
+using namespace std;
+
+struct cand {
+    string nome;
+    int nota;
+    int idade;
+};
+
+void merge(cand array[], int l, int r) {
+    cand temp[r + 1];
+
+    for (int i = l; i <= r; i++) {
+       temp[i].nome = array[i].nome;
+       temp[i].idade = array[i].idade;
+       temp[i].nota = array[i].nota;
+    }
+
+    int m = (l + r)/2;
+    int i1 = l;
+    int i2 = m + 1;
+        
+    for (int curr = l; curr <= r; curr++) {
+
+        if (i1 == (m + 1)) {
+            array[curr] = temp[i2++];
+        } 
+            
+        else if (i2 > r) { 
+            array[curr] = temp[i1++];
+        } 
+            
+        else if (temp[i1].nota < temp[i2].nota) {
+            array[curr] = temp[i2++];
+        } 
+
+        else if (temp[i1].nota > temp[i2].nota) {
+            array[curr] = temp[i1++];
+        }
+        
+            
+        else {
+
+            if (temp[i1].idade < temp[i2].idade) {
+                array[curr] = temp[i2++];
+            }
+            
+            else {
+                array[curr] = temp[i1++];
+            }
+        }
+    }
+}
+
+void mergesort(cand array[], int l, int r) {
+    if (l < r) {
+        int middle = (l + r)/2;
+        mergesort(array, l, middle);
+        mergesort(array, middle + 1, r);
+        merge(array, l, r);
+    }
+}
+
+int main() {
+    int cargos;
+    cin >> cargos;
+
+    for (int i = 0; i < cargos; i++) {
+        int num_cand;
+        int vagas;
+
+        cin >> num_cand >> vagas;
+        
+        cand candidato[num_cand];
+
+        for (int j = 0; j < num_cand; j++) {
+            cin >> candidato[j].nome >> candidato[j].nota >> candidato[j].idade;
+        }
+
+        mergesort(candidato, 0, num_cand - 1);
+
+        cout << "cargo " << i + 1 << ":" << endl;
+
+        for (int m = 0; m < vagas; m++) {
+            if (m >= num_cand) {
+                cout << "x" << endl;
+            }
+            
+            else {
+                cout << candidato[m].nome << endl;   
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ IUdex/ep2.c
@@ -0,0 +1,160 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct link {
+    int data;
+    struct link *next;
+
+} Link;
+
+
+Link *create_initial_link(Link *nextval) {
+    Link *node = (Link*)malloc(sizeof(Link));
+
+    node->next = nextval;
+
+    return node;
+}
+
+
+Link *create_more_links(Link *nextval, int it) {
+    Link *node = (Link*)malloc(sizeof(Link));
+
+    node->data = it;
+    node->next = nextval;
+
+    return node;
+}
+
+
+typedef struct {
+    Link *front;
+    Link *rear;
+    int size;
+    int cases;
+    int solved;
+
+} Queue;
+
+
+Queue *create_queue() {
+    Queue *queue = (Queue*)malloc(sizeof(Queue));
+
+    queue->front = queue->rear = create_initial_link(NULL);
+    queue->size = 0;
+    queue->cases = 0;
+    queue->solved = 0;
+
+    return queue;
+}
+
+
+void enqueue(Queue *queue, int it) { // add
+    queue->rear->next = create_more_links(NULL, it);
+    queue->rear = queue->rear->next;
+    queue->size++;
+}
+
+
+void dequeue(Queue *queue) { // solve
+    if (queue->size == 0) {
+        return;
+    }
+
+    Link *temp = queue->front->next;
+
+    queue->front->next = queue->front->next->next;
+
+    if (queue->front->next == NULL) {
+        queue->front = queue->rear;
+    }
+    
+    queue->size--;
+
+    free(temp);
+}
+
+
+void print_queue(Queue *queue) { // print
+    Link *temp = queue->front->next;
+
+    while (temp != NULL) {
+        if (temp->next != NULL) {
+            printf("%d ", temp->data);
+        }
+        
+        else {
+            printf("%d\n", temp->data);
+        }
+        
+        temp = temp->next;
+    }
+}
+
+/*
+minha função provisória de print
+
+void print_queue(Queue *queue) { // print
+    Queue *temp = queue;
+
+    while (temp->front->next != NULL) {
+        if (temp->front->next->data != queue->rear->data) {
+            printf("%d ", temp->front->next->data);
+        }
+        
+        else {
+            printf("%d\n", temp->front->next->data);
+        }
+        
+        temp->front = temp->front->next;
+    }
+}
+
+*/
+
+void status(int solve, int cases) { // status
+    printf("%d %d\n", solve, cases);
+}
+
+
+int main() {
+    int number;
+    char command[6];
+
+    Queue *queue = create_queue();
+
+    scanf(" %s", command);
+    while (strcmp(command, "end") != 0) {
+
+        if (strcmp(command, "add") == 0) {
+            scanf("%d", &number);
+            enqueue(queue, number);
+            queue->cases++;
+        }
+        
+        else if (strcmp(command, "solve") == 0) {
+            if (queue->size != 0) {
+                dequeue(queue);
+                queue->cases--;
+                queue->solved++;
+            }
+        }
+        
+        else if (strcmp(command, "print") == 0) {
+            if (queue->cases == 0) {
+                printf("x\n");
+            }
+            
+            else {
+                print_queue(queue);   
+            }
+        }
+        
+        else if (strcmp(command, "stats") == 0) {
+            status(queue->solved, queue->cases);
+        }
+
+        scanf("%s", command);
+    }
+}
