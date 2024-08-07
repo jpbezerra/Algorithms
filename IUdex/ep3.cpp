1a8d66a8cbec578dccf336db3251bab4bb744f5a
jpbezerra <jubalima2005@gmail.com>
2024-08-07 15:24:26 -0300

    Update ep3.cpp

--- IUdex/ep3.cpp
+++ IUdex/ep3.cpp
@@ -1,142 +1,124 @@
-#include <iostream>
-#include <string.h>
-#include <math.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 class Hash {
 private:
     int capacity;
-    int *HashTable;
-    int *index;
-    int *perm;
+    int currSize;
+    vector<int> hashTable;
+    vector<int> perm;
+    vector<int> index;
 
-    int HashKey(int value) {
+    int h(int value) {
         int temp = (int) floor((((double) value) / ((double) capacity)));
 
         return value - (capacity * temp);
+    };
+
+    int p(int i) {
+        return perm[i];
     }
 
-    int PseudoRandomProbing(int value) {
-        return perm[value - 1];
+    int find(int key) {
+        int tempKey = h(key);
+        int aux = tempKey;
+        int i = 1;
+
+        while (index[aux] != INT_MAX && i < 7) {
+            if (index[aux] == key) {
+                return aux;
+            }
+
+            aux = tempKey;
+            aux += p(i);
+            aux = aux % capacity;
+            i++;
+        }
+
+        return -1;
     }
 
 public:
     Hash(int size) {
         capacity = size;
+        currSize = 0;
 
-        HashTable = (int*)malloc(size * sizeof(int));
-        perm = (int*)malloc(size * sizeof(int));
-        index = (int*)malloc(size * sizeof(int));
+        hashTable.resize(size, -1);
+        perm.resize(size);
+        index.resize(size, INT_MAX);
 
-        int element;
-
-        for (int i = 0; i < capacity; i++) {
-            HashTable[i] = -1;
-            index[i] = -1;
-            
-
-            if (i != capacity - 1) {
-                cin >> element;
-                perm[i] = element;
-            }
+        perm[0] = 0;
+        for (int i = 1; i < size; i++) {
+            cin >> perm[i];
         }
-        
     };
-    
+
     ~Hash() {
-        free(HashTable);
-        free(perm);
+        hashTable.clear();
+        perm.clear();
+        index.clear();
     };
 
     void insert(int key, int value) {
-        int hold = key;
-        key = HashKey(key);
+        if (capacity > currSize && find(key) == -1) {
+            int tempKey = h(key);
+            int aux = tempKey;
+            int i = 1;
 
-        if (HashTable[key] == -1) {
-            HashTable[key] = value;
-            index[key] = hold;
-            return;
-        }
+            while (index[aux] != INT_MAX) {
+                aux = tempKey;
+                aux += p(i);
+                aux = aux % capacity;
+                i++;
+            }
 
-        int sup = 0;
-        while (HashTable[key] != -1 && sup != capacity - 1) {
-            sup++;
-            int curr = PseudoRandomProbing(sup);
-            key = (key + curr) % capacity;
-        }
+            index[aux] = key;
+            hashTable[aux] = value;
 
-        if (sup != capacity - 1) {
-            HashTable[key] = value;
-            index[key] = hold;
+            currSize++;
         }
     }
 
     void search(int key) {
-        int hold = key;
-        bool right = false;
-        key = HashKey(key);
+        int aux = find(key);
 
-        if (HashTable[key] == -1) {
-            cout << "-1" << endl;
+        if (aux == -1) {
+            cout << -1 << '\n';
             return;
         }
 
-        for (int i = 0; i < capacity; i++) {
-            if (index[i] == hold) {
-                cout << i << " " << HashTable[i] << endl;
-                right = true;
-            }
-        }
-        
-        if (!right) {
-            cout << "-1" << endl;
-        }
+        cout << aux << " " << hashTable[aux] << '\n';
     }
 };
 
 
 int main() {
-    bool running = true;
+    int numb, oper;
 
-    while (running) {
-        int number;
-        scanf("%d", &number);
+    cin >> numb;
+    while (numb != 0) {
+        Hash dict(numb);
+        cin >> oper;
 
-        if (number > 0) {
-            int j = 1;
-            Hash dict(number);
+        string operation;
+        int key, value;
 
-            int cases;
+        for (int i = 0; i < oper; i++) {
+            cin >> operation;
 
-            scanf("%d", &cases);
-
-            if (cases != 0) {
-                string operation;
-                int key;
-                int value;
-
-                for (int i = 0; i < cases; i++) {
-                    cin >> operation;
-
-                    if (operation == "add") {
-                        cin >> key >> value;
-
-                        dict.insert(key, value);
-                    }
-                        
-                    else if (operation == "find") {
-                        cin >> key;
-
-                        dict.search(key);
-                    }
-                }
+            if (operation == "add") {
+                cin >> key >> value;
+                dict.insert(key, value);
             }
-        }  
 
-        else {
-            running = false;
+            else {
+                cin >> key;
+                dict.search(key);
+            }
         }
+
+        cin >> numb;
     }
 
     return 0;
